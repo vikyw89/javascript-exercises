5a2a8fb162c1f5ad75d12bceed66081d81c9abea
vikyw89 <vikyw89@gmail.com>
2022-11-04 18:45:39 +0800

    6 done 7 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = (year) => {
+    if (year%100 === 0) {
+        return ((year/100)%4 === 0)
+    }
+    return (year % 4 === 0)
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = (temp) => {
+  let f = Math.round(((temp-32)/1.8)*10)/10
+  return f
 };
 
-const ctof = function() {
-
+const ctof = (temp) => {
+  let c = Math.round(((temp*1.8) + 32)*10)/10
+  return c
 };
 
 // Do not edit below this line
