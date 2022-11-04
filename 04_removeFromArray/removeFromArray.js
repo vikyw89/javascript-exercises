67ab55f1740fe7afbe1009119b182db0c71a2b7f
vikyw89 <vikyw89@gmail.com>
2022-11-04 18:26:22 +0800

    5 done 6 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = (arr,...args) => {
+    let output = arr
+    for(let i in args) {
+        output = output.filter(el => {
+            return el !== args[i]
+        })
+    }
+    return output
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = (start, end) => {
+    let output = 0
+    if (start > end) {
+        a = end;
+        b = start;
+    } else {
+        a = start;
+        b = end;
+    }
+    if ((start < 0) || (end < 0)) {
+        return 'ERROR'
+    }
+    if ((typeof start !== 'number') || (typeof end !== 'number')) {
+        return 'ERROR'
+    }
+    for (let i = a; i <= b; i++) {
+        output += i
+    }
+    return output
 };
 
 // Do not edit below this line
