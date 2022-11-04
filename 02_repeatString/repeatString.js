0f25b8c399948d2f3594ccaf84c1bc22dbdb9021
vikyw89 <vikyw89@gmail.com>
2022-11-04 17:39:37 +0800

    test 1 done test 2 done test 3 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = (word, times) => {
+    if (Number(times)<0) {
+        return 'ERROR'
+    }
+    let output = ""
+    for (let i = 0; i < Number(times); i++) {
+        output += word
+    }
+    return output
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = (words) => {
+    let output = ''
+    for(let i = words.length-1; i >= 0; i--) {
+        output += words[i]
+    }
+    return output
 };
 
 // Do not edit below this line
